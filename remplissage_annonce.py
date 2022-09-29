2e07ac794fec07ad9a1589d479cec0f801d922db
M-D-Q <maxdequick@gmail.com>
2022-09-29 10:07:40 +0200

    added first py file

---
+++ remplissage_annonce.py
@@ -0,0 +1,64 @@
+
+
+# 1 ère page
+
+""" Prénom
+Nom
+Email
+MDP
+BtnNext"""
+
+# 2ème page
+
+"""
+Search bar 'russe'
+wait
+click russe
+click boolean 'langue natale'
+4 clicks 'matières optionelles'
+BtnNext"""
+
+#3ème page
+
+"""
+text area 'titre de l'annonce'
+click BtnNext
+"""
+
+#4ème page
+
+"""
+text area "a propos du cours"
+Click BtnNext
+"""
+
+# 5ème page
+
+"""
+text area "a propos de vous"
+Click BtnNext
+"""
+
+# 6ème page
+
+"""
+CreationAddress 'Ville, Pays'
+button select hoverable x 3
+Click Btn Next
+"""
+
+#7ème page
+""" Button hoverable Anglais, Francais, Ukrainien, Russe
+Click BtnNext"""
+#8ème page
+""" input 25
+click btnnext"""
+#9ème page
+""" input 06785598774
+click btn next"""
+
+#10ème page
+""" ajout pdp jpg
+click btnnext"""
+
+#11ème page, accueil
