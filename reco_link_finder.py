1dd7b78eadd4d19c926e21dcac06a9b386fb1cc4
M-D-Q <maxdequick@gmail.com>
2022-10-20 17:05:23 +0200

    added json & py files

---
+++ reco_link_finder.py
@@ -0,0 +1,22 @@
+#### Fonction lancement du browser et get superprof.fr
+
+
+#### Fonction login
+#xpath champ adresse email : /html/body/div[2]/div[5]/div/div/div[2]/div/div[4]/div[1]/div[2]/form/div[1]/input
+#xpath champ password : /html/body/div[2]/div[5]/div/div/div[2]/div/div[4]/div[1]/div[2]/form/div[2]/input
+#xpath bouton connexion
+
+
+#Link pour annonces
+#https://www.superprof.fr/tableau-de-bord.html/annonces/liste/
+
+#Xpath du lien recommandation
+#/html/body/div[1]/div[2]/div/div/main/div/div[1]/div[7]/div[2]/div/input
+# A capturer et rnetrer dans le json
+
+#Xpath du bouton "voir en tant qu'eleve"
+#/html/body/div[1]/div[2]/div/div/main/div/div[2]/div[2]/div[2]/a
+
+#Xpath du texte 'nombre d'avis'
+#/html/body/div[2]/div[2]/div/div[1]/div/div[2]/div[2]/div[4]/div[1]/div[2]/span[2]
+# A capturer et rentrer dans le json
